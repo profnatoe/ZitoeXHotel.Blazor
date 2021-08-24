c82efcc9ae930737bf6a1b4dafed51e10a3dcd6d
profnatoe <sitoenando@outlook.com>
2021-08-24 12:23:21 +0200

    Added Model and Changed the index page

---
+++ ZitoeXHotel.Server/Data/Room.cs
@@ -0,0 +1,13 @@
+﻿namespace ZitoeXHotel.Server.Data
+{
+      public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
