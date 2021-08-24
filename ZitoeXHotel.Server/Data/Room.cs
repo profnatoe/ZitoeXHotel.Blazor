43c4fb014473929b788d089cebada8bb480a9d67
profnatoe <sitoenando@outlook.com>
2021-08-24 13:06:08 +0200

    Added RoomList and a table for viewing room and its properties

--- ZitoeXHotel.Server/Data/Room.cs
+++ ZitoeXHotel.Server/Data/Room.cs
@@ -1,4 +1,6 @@
-﻿namespace ZitoeXHotel.Server.Data
+﻿using System.Collections.Generic;
+
+namespace ZitoeXHotel.Server.Data
 {
       public class Room
     {
@@ -6,8 +8,15 @@
         public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
-
+        public ICollection<RoomProperty> RoomProperties { get; set; }
         public bool IsActive { get; set; }
 
+    }
+
+    public class RoomProperty
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
     }
 }
